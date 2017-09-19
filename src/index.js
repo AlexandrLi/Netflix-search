b24870f586618a98ffedf29ab097064aa2ffbd6f
Alexandr_Li <Alexandr_li@epam.com>
2017-09-19 08:52:21 +0600

    Task 1. Setting up the environment

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as s from './styles/style.css';
+import { App } from './app/App.jsx';
+
+ReactDOM.render( <App /> , document.getElementById('container'));
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+        ],
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              camelCase: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.css']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './index.html'
+    })
+  ],
+};
