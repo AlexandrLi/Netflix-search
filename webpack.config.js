070a9abcfecd30a926a86431ae5c05ca1cb220a7
Alexandr Li <muzikant1990@gmail.com>
2017-11-06 23:51:35 +0600

    fix errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const webpack = require('webpack');
 
 module.exports = {
   devtool: 'cheap-module-source-map',
